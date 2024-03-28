325ea3ccb7fe1c1b4c698f773ac11c044a858f56
Kleber Stender <kleber.stender@gmail.com>
2024-03-28 13:14:29 +0100

    [Task-1] Added simple explanation

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# In order to properly run the program, run sbt test first to have the below file placed in the correctly place, or pass another url
 sbt "main/run 03-delivery/target/scala-2.13/test-classes/test_csv"
